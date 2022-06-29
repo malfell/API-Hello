64e41594daf123dbb1582717125606e83b142175
malfell <malfellows13@gmail.com>
2022-06-28 19:34:40 -0500

    created language schema

---
+++ models/language.js
@@ -0,0 +1,18 @@
+//require mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+//create languageSchema
+const languageSchema = new Schema({
+    name: {type: String, required: true},
+    greeting: String,
+    pangram: String,
+    filler: String,
+
+})
+
+
+//create and export model
+const Language = mongoose.model('Language', languageSchema)
+module.exports = Language
